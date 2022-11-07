8f1c36a2fb36d100d228f136db2f2c03075975b0
LocDayNha <vohoangloc200@gmail.com>
2022-11-07 14:07:09 +0700

    add layout
    :>>>>>

---
+++ app/src/main/java/com/example/pro1121/view/DatXeActivity.java
@@ -0,0 +1,17 @@
+package com.example.pro1121.view;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.pro1121.R;
+
+
+public class DatXeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dat_xe);
+    }
+}
