30b3fd8fca858ccb3ab7ab24715161d7f74ec136
Nguyễn Tấn Thy <96558592+thynt27@users.noreply.github.com>
2022-11-11 08:24:54 +0700

    applyfragmentmain

--- app/src/main/java/com/example/pro1121/view/MainActivity.java
+++ app/src/main/java/com/example/pro1121/view/MainActivity.java
@@ -2,13 +2,27 @@ package com.example.pro1121.view;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.FrameLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
+import com.example.pro1121.Fragment.DatVeFragment;
+import com.example.pro1121.Fragment.NguoiDungFragment;
+import com.example.pro1121.Fragment.TinTucFragment;
+import com.example.pro1121.Fragment.TramXeFragment;
+import com.example.pro1121.Fragment.VeXeFragment;
 import com.example.pro1121.R;
 import com.facebook.login.Login;
 import com.facebook.login.LoginManager;
@@ -21,38 +35,98 @@ import com.google.android.gms.common.api.ResultCallback;
 import com.google.android.gms.common.api.Status;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
     Button btnLogOut;
     GoogleApiClient mGoogleApiClient;
+    DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        btnLogOut = findViewById(R.id.btnLogOut);
+        Toolbar toolbar = findViewById(R.id.toolBar);
+        FrameLayout layout = findViewById(R.id.frLayout);
+        NavigationView navigationView = findViewById(R.id.navView);
+        drawerLayout = findViewById(R.id.drawerLayout);
+        View headerLayout = navigationView.getHeaderView(0);
+        TextView txtTen = headerLayout.findViewById(R.id.txtTen);
 
-        btnLogOut.setOnClickListener(new View.OnClickListener() {
+        setSupportActionBar(toolbar);
+        ActionBar bar = getSupportActionBar();
+        bar.setDisplayHomeAsUpEnabled(true);
+
+        bar.setHomeAsUpIndicator(R.drawable.ic_menu);
+
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
             @Override
-            public void onClick(View view) {
-                //Logout google
-                Auth.GoogleSignInApi.signOut(mGoogleApiClient).setResultCallback(
-                        new ResultCallback<Status>() {
-                            @Override
-                            public void onResult(Status status) {
-                                // ...
-                                Toast.makeText(getApplicationContext(),"Logged Out",Toast.LENGTH_SHORT).show();
-                                Intent i=new Intent(getApplicationContext(),LoginActivity.class);
-                                startActivity(i);
-                            }
-                        });
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment fragment = null;
+                switch (item.getItemId()) {
+                    case R.id.mDatVe:
+                        fragment = new DatVeFragment();
+                        break;
+                    case R.id.mThongTinTramXe:
+                        fragment = new TramXeFragment();
+                        break;
+                    case R.id.mThongTinVeXe:
+                        fragment = new VeXeFragment();
+                        break;
+                    case R.id.mTinTuc:
+                        fragment = new TinTucFragment();
+                        break;
+                    case R.id.mThoat:
+                        //Logout google
+                        Auth.GoogleSignInApi.signOut(mGoogleApiClient).setResultCallback(
+                                new ResultCallback<Status>() {
+                                    @Override
+                                    public void onResult(Status status) {
+                                        // ...
+                                        Toast.makeText(getApplicationContext(),"Logged Out",Toast.LENGTH_SHORT).show();
+                                        Intent i=new Intent(getApplicationContext(),LoginActivity.class);
+                                        //Sau khi out khỏi account sẽ trở về màn hình điện thoại, không trở về main
+                                        i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                                        startActivity(i);
+                                    }
+                                });
 
-                //Logout facebook
-                LoginManager.getInstance().logOut();
+                        //Logout facebook..
+                        LoginManager.getInstance().logOut();
+
+                    case R.id.mThongTinNguoiDung:
+                        fragment = new NguoiDungFragment();
+                        break;
+
+                    default:
+                        fragment = new DatVeFragment();
+                }
+                if (fragment != null){
+                    FragmentManager fragmentManager = getSupportFragmentManager();
+                    fragmentManager.beginTransaction()
+                            .replace(R.id.frLayout, fragment)
+                            .commit();
+                    toolbar.setTitle(item.getTitle()); // lay ten
+                }
+
+                drawerLayout.closeDrawer(GravityCompat.START);
+
+                return false;
             }
         });
+
+
+
+    }
+    //Sự kiện cho nút menu
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            drawerLayout.openDrawer(GravityCompat.START);
+        }
+        return super.onOptionsItemSelected(item);
 
     }
 
