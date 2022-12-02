0d73afb6b5af65ddd284e917c38a80d9b3c46fe2
NguyenHuuHoa29 <hoacrkh10@gmail.com>
2022-12-02 11:36:24 +0700

    datvenebaniu

---
+++ app/src/main/java/com/example/pro1121/model/VeXE.java
@@ -0,0 +1,54 @@
+package com.example.pro1121.model;
+
+public class VeXE {
+    int id;
+    String idvexe,vitri,gia,soluong;
+
+    public VeXE(int id,  String vitri, String gia, String soluong) {
+        this.id = id;
+
+        this.vitri = vitri;
+        this.gia = gia;
+        this.soluong = soluong;
+    }
+
+    public String getIdvexe() {
+        return idvexe;
+    }
+
+    public void setIdvexe(String idvexe) {
+        this.idvexe = idvexe;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getVitri() {
+        return vitri;
+    }
+
+    public void setVitri(String vitri) {
+        this.vitri = vitri;
+    }
+
+    public String getGia() {
+        return gia;
+    }
+
+    public void setGia(String gia) {
+        this.gia = gia;
+    }
+
+    public String getSoluong() {
+        return soluong;
+    }
+
+    public void setSoluong(String soluong) {
+        this.soluong = soluong;
+    }
+}
