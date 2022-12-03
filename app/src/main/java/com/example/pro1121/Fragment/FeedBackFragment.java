fc193a9c845aca0fbf9593b082cdf8d766ab072b
LocDayNha <vohoangloc200@gmail.com>
2022-12-03 13:39:44 +0700

    sửa feedback and news

--- app/src/main/java/com/example/pro1121/Fragment/FeedBackFragment.java
+++ app/src/main/java/com/example/pro1121/Fragment/FeedBackFragment.java
@@ -1,12 +1,14 @@
 package com.example.pro1121.Fragment;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -14,16 +16,21 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.bumptech.glide.Glide;
+import com.example.pro1121.HuongDan.LoadActivity;
 import com.example.pro1121.R;
+import com.example.pro1121.view.MainActivity;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class FeedBackFragment extends Fragment {
-    private Button btnFeedBack;
+    private Button btnFeedBack, btnGetFeedBack;
     private EditText edtFeedBack, edtFeedBackEmail, edtSDT;
+    private TextView get_data1, get_data2, get_data3;
 
     @Nullable
     @Override
@@ -31,9 +38,13 @@ public class FeedBackFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_feedback, container, false);
 
         btnFeedBack = view.findViewById(R.id.btnFeedBack);
+        btnGetFeedBack = view.findViewById(R.id.btnGetFeedBack);
         edtFeedBack = view.findViewById(R.id.edtFeedBack);
         edtFeedBackEmail = view.findViewById(R.id.edtFeedBackEmail);
         edtSDT = view.findViewById(R.id.edtSDT);
+        get_data1 = view.findViewById(R.id.get_data1);
+        get_data2 = view.findViewById(R.id.get_data2);
+        get_data3 = view.findViewById(R.id.get_data3);
 
         setProFile();
 
@@ -41,6 +52,13 @@ public class FeedBackFragment extends Fragment {
             @Override
             public void onClick(View view) {
                 onClickFeedBack();
+            }
+        });
+
+        btnGetFeedBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                readDatabase();
             }
         });
 
@@ -65,14 +83,64 @@ public class FeedBackFragment extends Fragment {
         });
     }
 
+    private void readDatabase(){
+        String feedback ="feedback/";
+        int id=0;
+        id+=1;
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+
+        DatabaseReference myRef = database.getReference( feedback + id + "/email");
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                String value = dataSnapshot.getValue(String.class);
+                get_data1.setText(value);
+            }
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+            }
+        });
+
+        DatabaseReference myRef1 = database.getReference( feedback + id + "/phone");
+        myRef1.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                String value = dataSnapshot.getValue(String.class);
+                get_data2.setText(value);
+            }
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+            }
+        });
+
+        DatabaseReference myRef2 = database.getReference( feedback + id + "/cmt");
+        myRef2.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                String value = dataSnapshot.getValue(String.class);
+                get_data3.setText(value);
+            }
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+            }
+        });
+    }
+
     private void setProFile(){
         FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
         if (user == null){
             return;
         }
         edtFeedBackEmail.setText(user.getEmail());
-        edtSDT.setText(user.getPhoneNumber());
-        Glide.with(getActivity()).load(user.getPhotoUrl()).error(R.drawable.ic_avatar);
         Log.d("<><><><><>TAG", "setProFile: ");
     }
 
